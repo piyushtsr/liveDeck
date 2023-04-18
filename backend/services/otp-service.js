ed49240e75ef5d98d5c693d8feb306f6114443c0
piyush77 <piyushrahangdale21@gmail.com>
2023-04-18 20:23:07 +0530

    adding backend

---
+++ backend/services/otp-service.js
@@ -0,0 +1,31 @@
+const crypto = require('crypto');
+const hashService = require('./hash-service');
+
+const smsSid = process.env.SMS_SID;
+const smsAuthToken = process.env.SMS_AUTH_TOKEN;
+const twilio = require('twilio')(smsSid, smsAuthToken, {
+    lazyLoading: true,
+});
+
+class OtpService {
+    async generateOtp() {
+        const otp = crypto.randomInt(1000, 9999);
+        return otp;
+    }
+
+    async sendBySms(phone, otp) {
+        return await twilio.messages.create({
+            body: `Your LiveDeck OTP is ${otp}`,
+            from: process.env.SMS_FROM_NUMBER,
+            to: phone,
+            
+        });
+    }
+
+    verifyOtp(hashedOtp, data) {
+        let computedHash = hashService.hashOtp(data);
+        return computedHash === hashedOtp;
+    }
+}
+
+module.exports = new OtpService();
